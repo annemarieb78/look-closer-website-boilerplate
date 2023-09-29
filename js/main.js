5e64ba46f8a2ff487eb97d1548f045b04eaa0795
Anne-M <77175884+annemarieb78@users.noreply.github.com>
2023-09-28 21:10:46 -0600

    Added clear form js

--- js/main.js
+++ js/main.js
@@ -64,6 +64,21 @@ function autoScroll() {
 setup();
 setInterval(autoScroll, autoScroll_interval);
 
+/*signup uptop form*/
+function SubForm() {
+  $.ajax({
+    url: "https://api.apispreadsheets.com/data/viXsgKp9LABNAE2N/",
+    type: "post",
+    data: $("#myForm-uptop").serializeArray(),
+    // success: function () {
+    //   alert("Form Data Submitted :)");
+    // },
+    // error: function () {
+    //   alert("There was an error :(");
+    // },
+  });
+}
+
 /*signup form*/
 function SubForm() {
   $.ajax({
@@ -79,22 +94,18 @@ function SubForm() {
   });
 }
 
-/*signup uptop form*/
-function SubForm() {
-  $.ajax({
-    url: "https://api.apispreadsheets.com/data/viXsgKp9LABNAE2N/",
-    type: "post",
-    data: $("#myForm-uptop").serializeArray(),
-    // success: function () {
-    //   alert("Form Data Submitted :)");
-    // },
-    // error: function () {
-    //   alert("There was an error :(");
-    // },
-  }); 
-}
-
-function clearForm() {
-  document.getElementByName("name").value = "";
-  document.getElementByName("email").value = "";
+/*Clear Form*/
+let output = document.getElementById("output");
+function clearText(event) {
+  // access input field
+  let input1 = document.getElementById("input1");
+  let input2 = document.getElementById("input2");
+  let input3 = document.getElementById("input3");
+  let input4 = document.getElementById("input4");
+  // clear the input field.
+  input1.value = "";
+  input2.value = "";
+  input3.value = "";
+  input4.value = "";
+  // output.innerHTML += "Form submitted successfully! <br>";
 }
